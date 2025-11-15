34108ae9bdf11ff5492dc5eaee5ca378fb35fc2c
vinss-droid <kevinsipahutar220604@gmail.com>
2025-11-15 22:25:27 +0700

    fix(route): fix import sequelize

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { sequelize } = require("../models");
 
 const userRoutes = require("./userRoute");
 const serviceRoutes = require("./serviceRoute");
