3d5d22148f1bb06f89afb0a639042451407ee5b7
TegarAlfaR <tegaralfa18052004@gmail.com>
2025-11-19 20:26:33 +0700

    fix(route): change authorization in route folder

--- config/database.js
+++ config/database.js
@@ -8,12 +8,7 @@ module.exports = {
         host: process.env.DB_HOST,
         dialect: process.env.DB_DIALECT,
         port: process.env.DB_PORT,
-        dialectOptions: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false
-            }
-        }
+        
     },
 
     test: {
